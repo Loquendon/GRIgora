d35badeea2e2b2ede9201e3119df0f396982c2f4
Loquendon <99416043+Loquendon@users.noreply.github.com>
2020-12-13 12:36:22 +0000

    clean up a little

--- src/com/ollie/controller/LoginController.java
+++ src/com/ollie/controller/LoginController.java
@@ -1,2 +1,20 @@
 
 package com.ollie.controller;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import com.ollie.model.Login;
+
+public class LoginController {
+	
