b46b4b8321f7c610949e58432a83a063cf84c550
Loquendon <99416043+Loquendon@users.noreply.github.com>
2021-01-03 11:50:04 +0000

    v3.4

--- src/com/ollie/controller/LoginController.java
+++ src/com/ollie/controller/LoginController.java
@@ -39,3 +39,26 @@ public class LoginController {
             //Change scene 
             
             Parent mainView = FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
+
+            Scene mainScene = new Scene(mainView, 1200, 800);
+
+            Stage mainStage = new Stage();
+
+            mainStage.setTitle("FX Trade");
+
+            mainStage.setScene(mainScene);
+
+            mainStage.show();
+
+            Stage stage = (Stage) userF.getScene().getWindow();
+            stage.close();
+
+
+        } 
+    	else {
+        	   	
+        errorLabel.setText("Error Please Try Again");
+       
+    }
+
+}
