ec43cf59d34a6eda467b5c354d2cf19d1756054e
Loquendon <99416043+Loquendon@users.noreply.github.com>
2021-04-08 10:27:28 +0000

    german translations

--- src/com/ollie/model/CurrentGraph.java
+++ src/com/ollie/model/CurrentGraph.java
@@ -4,3 +4,21 @@ package com.ollie.model;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
+
+import com.oanda.fxtrade.api.FXClient;
+
+
+public class CurrentGraph {
+	
+	private static Long currentGraphInterval = FXClient.INTERVAL_5_SEC;
+	private static int currentGraphPointCount = 75;
+	private static boolean updateInProgress = false;
+	
+	public static Long getCurrentGraphInterval(){
+		return currentGraphInterval;
+	}
+	public static void setCurrentGraphInterval(Long l){
+		currentGraphInterval = l;
+	}
+	public static int getCurrentGraphPointCount(){
+		return currentGraphPointCount;
