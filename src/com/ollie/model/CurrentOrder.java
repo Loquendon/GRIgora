854810fd8ab7833f835e7c345350b0ae0cfd7dac
Loquendon <99416043+Loquendon@users.noreply.github.com>
2021-04-11 05:31:42 +0000

    update relnotes

--- src/com/ollie/model/CurrentOrder.java
+++ src/com/ollie/model/CurrentOrder.java
@@ -5,4 +5,5 @@ import java.util.Vector;
 import com.oanda.fxtrade.api.API;
 import com.oanda.fxtrade.api.FXPair;
 import com.oanda.fxtrade.api.Account;
-import com.oanda.fxtrade.api.FXClien
+import com.oanda.fxtrade.api.FXClient;
+import com.oanda.fxtrad
