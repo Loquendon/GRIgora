14d6cf7db41ed150602a07ffe9cc7779cbdeadb0
Loquendon <99416043+Loquendon@users.noreply.github.com>
2021-04-13 20:15:04 +0000

    talk

--- src/com/ollie/model/CurrentOrder.java
+++ src/com/ollie/model/CurrentOrder.java
@@ -10,4 +10,5 @@ import com.oanda.fxtrade.api.MarketOrder;
 import com.oanda.fxtrade.api.OAException;
 import com.oanda.fxtrade.api.StopLossOrder;
 import com.oanda.fxtrade.api.TakeProfitOrder;
-import com.oan
+import com.oanda.fxtrade.api.User;
+im
