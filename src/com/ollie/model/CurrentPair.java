d6c704bc3d35e6f85d2c2f065aca0914e5863d23
Loquendon <99416043+Loquendon@users.noreply.github.com>
2021-10-22 09:29:15 +0000

    Adjust code style

--- src/com/ollie/model/CurrentPair.java
+++ src/com/ollie/model/CurrentPair.java
@@ -12,4 +12,5 @@ public class CurrentPair {
 
 	private static String currentPair = "GBP/USD";
 	private static FXPair p;
-	private static Vector<FXHistoryPoint> fiveSecVector = new Vector<FXHist
+	private static Vector<FXHistoryPoint> fiveSecVector = new Vector<FXHistoryPoint>();
+	private
