37f257a8e2237461bf53452b30c6ec2ff543ab3f
Loquendon <99416043+Loquendon@users.noreply.github.com>
2021-11-04 14:02:47 +0000

    Better comments

--- src/com/ollie/model/CurrentPair.java
+++ src/com/ollie/model/CurrentPair.java
@@ -16,4 +16,4 @@ public class CurrentPair {
 	private static Vector<FXHistoryPoint> thirtySecVector = new Vector<FXHistoryPoint>();
 	private static Vector<FXHistoryPoint> oneMinVector = new Vector<FXHistoryPoint>();
 	private static Vector<FXHistoryPoint> fiveMinVector = new Vector<FXHistoryPoint>();
-	private static Vector<FXHistory
+	private static Vector<FXHistoryPoint> oneHourV
