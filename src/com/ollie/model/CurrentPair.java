b65cd92d1815929489fb6546caa16ba3235757f3
Loquendon <99416043+Loquendon@users.noreply.github.com>
2021-12-26 16:15:15 +0000

    Pontoon: Update Dutch (nl) localization of Test Pilot Website

--- src/com/ollie/model/CurrentPair.java
+++ src/com/ollie/model/CurrentPair.java
@@ -52,4 +52,4 @@ public class CurrentPair {
 				
 				fiveSecVector.addAll(fxclient.getRateTable().getHistory(p, FXClient.INTERVAL_5_SEC, CurrentGraph.getCurrentGraphPointCount()));
 				thirtySecVector.addAll(fxclient.getRateTable().getHistory(p, FXClient.INTERVAL_30_SEC, CurrentGraph.getCurrentGraphPointCount()));
-				oneMinVector.addAll(fxclient.getRateTable().getHistory(p, FXClient.INTERVAL_1_MIN, CurrentGraph.getCurr
+				oneMinVector.addAll(fxclient.getRateTable().getHistory(p, FXClient.INTERVAL_1_MIN, CurrentGraph.getCurrentGraphPointCount())
