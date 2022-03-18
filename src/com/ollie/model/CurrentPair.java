7e95352e6ab001104a4bb58bbf90e35d2fb57386
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-03-18 12:29:38 +0000

    Update gutenberg-mobile reference

--- src/com/ollie/model/CurrentPair.java
+++ src/com/ollie/model/CurrentPair.java
@@ -86,4 +86,4 @@ public class CurrentPair {
 			}
 			else if(interval == FXClient.INTERVAL_30_SEC){
 				thirtySecVector.clear();
-				thirtySecVector.addAll(fxclient.getRateTab
+				thirtySecVector.addAll(fxclient.getRateTable().getHistory(p, FX
