cb811fd7856f3acb77234b764efb727e3913cce4
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-04-30 07:40:52 +0000

    update sample config

--- src/com/ollie/model/GraphUpdater.java
+++ src/com/ollie/model/GraphUpdater.java
@@ -3,4 +3,5 @@ package com.ollie.model;
 import java.util.Vector;
 
 import com.oanda.fxtrade.api.API;
-import com.oanda.fxtrade.api.FXHistoryP
+import com.oanda.fxtrade.api.FXHistoryPoint;
+import com
