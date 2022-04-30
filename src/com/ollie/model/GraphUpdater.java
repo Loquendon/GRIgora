a8b1c7f28246e81ab46562fb441183192ca3152e
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-04-30 10:45:21 +0000

    Ongoing development

--- src/com/ollie/model/GraphUpdater.java
+++ src/com/ollie/model/GraphUpdater.java
@@ -4,4 +4,6 @@ import java.util.Vector;
 
 import com.oanda.fxtrade.api.API;
 import com.oanda.fxtrade.api.FXHistoryPoint;
-import com.ollie.controller.MainContr
+import com.ollie.controller.MainController;
+
+import jav
