314a0d1dba9a9e9c05b5b8531430734238a0e2a2
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-05-27 09:52:44 +0000

    Add `.editorconfig`

--- src/com/ollie/model/GraphUpdater.java
+++ src/com/ollie/model/GraphUpdater.java
@@ -26,4 +26,4 @@ public class GraphUpdater implements  Runnable {
         	if( cmp < 0 || cmp > 0 || !interval.equals(CurrentGraph.getCurrentGraphInterval())){
         		
         		MainController.initGraph();
-   
+        		interval = Current
