a4763dfdf4db962d187d1e41dce38f62223ecfa3
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-06-18 12:28:57 +0000

    add travis yml file

--- src/com/ollie/model/Login.java
+++ src/com/ollie/model/Login.java
@@ -5,3 +5,24 @@ import java.util.Vector;
 
 import com.oanda.fxtrade.api.API;
 import com.oanda.fxtrade.api.Account;
+import com.oanda.fxtrade.api.AccountException;
+import com.oanda.fxtrade.api.FXClient;
+import com.oanda.fxtrade.api.InvalidPasswordException;
+import com.oanda.fxtrade.api.InvalidUserException;
+import com.oanda.fxtrade.api.MultiFactorAuthenticationException;
+import com.oanda.fxtrade.api.SessionException;
+import com.oanda.fxtrade.api.User;
+
+public class Login {
+
+	private static FXClient fxclient;
+	
+	public static int loginAttempt(String user, String pass){
+		
+		fxclient = API.createFXGame();
+		fxclient.setWithRateThread(true); 
+		
+		System.out.println("login attempt with user " + user);
+		
+		try { fxclient.login(user, pass); }
+		catch (SessionException e) { return -1; }
