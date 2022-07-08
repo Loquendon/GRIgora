60766491fc47e4e12db2eb6d978480275c18fcdc
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-07-08 06:27:14 +0000

    v0.8.6

--- src/com/ollie/model/OpenOrders.java
+++ src/com/ollie/model/OpenOrders.java
@@ -21,4 +21,4 @@ import javafx.scene.paint.Color;
 public class OpenOrders {
 
 	private static Vector<MarketOrder> trades = new Vector<MarketOrder>();
-	private static Ar
+	private static ArrayList<But
