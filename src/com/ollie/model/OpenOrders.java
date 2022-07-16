2c0def28899496f4b35d8f588d6bb9ce666975cc
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-07-16 14:41:16 +0000

    Updates to package.json

--- src/com/ollie/model/OpenOrders.java
+++ src/com/ollie/model/OpenOrders.java
@@ -26,4 +26,5 @@ public class OpenOrders {
 	private static ArrayList<String> time = new ArrayList<String>();
 	private static ArrayList<String> units = new ArrayList<String>();
 	private static ArrayList<String> pair = new ArrayList<String>();
-	private static ArrayList<String> price = new Ar
+	private static ArrayList<String> price = new ArrayList<String>();
+	private
