0d4dcddce773aeb25fac589d37b034bceb7bcb8a
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-08-06 18:42:11 +0000

    asdsad

--- src/com/ollie/model/OpenOrders.java
+++ src/com/ollie/model/OpenOrders.java
@@ -34,4 +34,5 @@ public class OpenOrders {
 
 	
 	@SuppressWarnings("unchecked")
-	public static void updateOpenOrders() throws SessionException
+	public static void updateOpenOrders() throws SessionException{
+		trades.clear();
