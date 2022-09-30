d7db34e7417dcd8625b3b2386ff4989ab081cfb0
Loquendon <99416043+Loquendon@users.noreply.github.com>
2022-09-30 11:25:47 +0000

    Create .gitignore

--- src/com/ollie/model/OpenOrders.java
+++ src/com/ollie/model/OpenOrders.java
@@ -99,4 +99,4 @@ public class OpenOrders {
 			
 			try {
 				DecimalFormat df = new DecimalFormat("#.#####");
-				String pl = df.format(mo.getUnreali
+				String pl = df.format(mo.getUnrealizedPL(fxclient.getRateT
